cb8ddb98fca46e279f78aeb3c9dc42930fe02338
gideong <gideong@yahoo-inc.com>
2015-09-14 11:17:48 -0700

    Twitter login. URL scheme.

--- twitter-client/ViewController.swift
+++ twitter-client/ViewController.swift
@@ -23,10 +23,12 @@ class ViewController: UIViewController {
         TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET",
             callbackURL: NSURL(string:"cptwitterdemo://oauth"), scope: nil,
             success: { (requestToken: BDBOAuth1Credential!) -> Void in
-                print("Got ze token")
+                print("Got token")
+                let authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")!
+                UIApplication.sharedApplication().openURL(authURL)
             },
             failure: { (error: NSError!) -> Void in
-                print("Got FAIL")
+                print("Failed to get token")
             }
         )
     }
