b5817ee56d17cacab762c3859d342ed5c2bb366a
gideong <gideong@yahoo-inc.com>
2015-09-14 10:32:44 -0700

    Singleton TwitterClient.

--- twitter-client/TwitterClient.swift
+++ twitter-client/TwitterClient.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+let twitterConsumerKey = "SucVaBdt4JzDK2d1i2oLvGHVq"
+let twitterConsumerSecret = "y6EYS9N8WoYwG50aX5WyCPfiu5nQRRg0yKWzxkY0FrMQkt5Cr5"
+let twitterBaseUrl = NSURL(string: "https://api.twitter.com")
+
 class TwitterClient: BDBOAuth1RequestOperationManager {
+
+    static let sharedInstance = TwitterClient(baseURL: twitterBaseUrl, consumerKey: twitterConsumerKey,
+        consumerSecret: twitterConsumerSecret)
 
 }
--- twitter-client/ViewController.swift
+++ twitter-client/ViewController.swift
@@ -8,16 +8,10 @@
 
 import UIKit
 
-let twitterConsumerKey = "SucVaBdt4JzDK2d1i2oLvGHVq"
-let twitterConsumerSecret = "y6EYS9N8WoYwG50aX5WyCPfiu5nQRRg0yKWzxkY0FrMQkt5Cr5"
-let twitterBaseUrl = NSURL(string: "https://api.twitter.com")
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        TwitterClient(baseURL: twitterBaseUrl, consumerKey: twitterConsumerKey,
-                consumerSecret: twitterConsumerSecret)
     }
 
     override func didReceiveMemoryWarning() {
