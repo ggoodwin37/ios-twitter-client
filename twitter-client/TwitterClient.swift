963bbd425179bfbc50bc35f4e2a6130fd6648c62
gideong <gideong@yahoo-inc.com>
2015-09-14 10:25:07 -0700

    WIP

---
+++ twitter-client/TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  twitter-client
+//
+//  Created by Gideon Goodwin on 9/14/15.
+//  Copyright © 2015 Gideon Goodwin. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1RequestOperationManager {
+
+}
